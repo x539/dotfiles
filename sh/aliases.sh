ad0b1c498cb09a110a9d6a9894913e6a4b06097b
Andreas Freimuth <andreas.freimuth@united-bits.de>
2018-04-11 11:41:51 +0200

    add aliasse

--- sh/aliases.sh
+++ sh/aliases.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+alias dc='docker-compose'
+alias g='git'
 alias ll='ls -l'
 alias sudo='sudo '
+
+if command -v tmux >/dev/null 2>&1; then
+	alias tmux='tmux -f ${XDG_CONFIG_HOME}/tmux/tmux.conf'
+fi
