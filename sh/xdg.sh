b953f67a5ccdb2c9a1f9fdfaa2efaa5495af8668
Andreas Freimuth <andreas.freimuth@frmth.de>
2024-08-24 13:48:24 +0200

    feat(xdg): store history files in XDG_STATE_HOME

--- sh/xdg.sh
+++ sh/xdg.sh
@@ -4,11 +4,15 @@
 : ${XDG_CACHE_HOME:="$HOME/.cache"}
 : ${XDG_CONFIG_HOME:="$HOME/.config"}
 : ${XDG_DATA_HOME:="$HOME/.local/share"}
+: ${XDG_STATE_HOME:="$HOME/.local/state"}
 
-export XDG_BIN_HOME XDG_CACHE_HOME XDG_CONFIG_HOME XDG_DATA_HOME
+export XDG_BIN_HOME XDG_CACHE_HOME XDG_CONFIG_HOME XDG_DATA_HOME XDG_STATE_HOME
 
-export PATH="$XDG_BIN_HOME:$PATH"
+[[ $PATH =~ $XDG_BIN_HOME ]] || export PATH="$XDG_BIN_HOME:$PATH"
 export INPUTRC="$XDG_CONFIG_HOME/inputrc"
-export TMUX_TMPDIR="${XDG_RUNTMIME_DIR}"
+export TMUX_TMPDIR="$XDG_RUNTMIME_DIR"
+export HISTFILE="$XDG_STATE_HOME/history/bash"
+export PSQL_HISTORY="$XDG_STATE_HOME/history/psql"
+export PYTHON_HISTFILE="$XDG_STATE_HOME/history/python"
 
 source $XDG_CONFIG_HOME/sh/profile.sh
